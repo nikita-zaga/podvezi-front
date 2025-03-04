10594bd41c0ac8f990b5c4afa7f3c1c824c487b9
ZagainovNA <n.zagainov@id-mt.ru>
2025-03-04 15:32:43 +0300

    add new section for drivers while registration

--- src/composables/useRegistration.js
+++ src/composables/useRegistration.js
@@ -7,21 +7,41 @@ export function useRegistration() {
     const selectedRole = ref("");
     const router = useRouter();
 
+    const driverInfo = ref({
+        experience: null,
+        car: {
+            model: "",
+            number: "",
+            color: "",
+        },
+    });
+
+
     const handleSubmit = async () => {
+        console.log("Начало handleSubmit");
         if (!selectedRole.value) {
             alert("Выберите роль перед регистрацией!");
             return;
+        }
+
+        console.log("Отправляемые данные:", driverInfo.value);
+        // Формируем объект для отправки
+        const body = {
+            username: username.value,
+            password: password.value,
+            role: selectedRole.value.id,
+        };
+
+        if (selectedRole.value.id === "driver") {
+            body.experience = driverInfo.value.experience;
+            body.car = driverInfo.value.car;
         }
 
         try {
             const response = await fetch("http://localhost:8080/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    username: username.value,
-                    password: password.value,
-                    role: selectedRole.value.id,
-                }),
+                body: JSON.stringify(body),
             });
 
             if (response.ok) {
@@ -32,9 +52,10 @@ export function useRegistration() {
                 alert(`Ошибка: ${error.message || "Не удалось зарегистрироваться"}`);
             }
         } catch (err) {
+            console.log("Ошибка при регистрации", err);
             alert("Ошибка сервера");
         }
     };
 
-    return { username, password, selectedRole, handleSubmit };
+    return { username, password, selectedRole, driverInfo, handleSubmit };
 }
